b25355aa1a5aa1cfd90367b7267ec4d0d6c63825
MR-Liakos <saltsidisilia@gmail.com>
2025-01-25 16:09:15 +0200

    add/user model

--- Project/Backend/server/server/settings.py
+++ Project/Backend/server/server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'userauth'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'userauth.CustomUser'
