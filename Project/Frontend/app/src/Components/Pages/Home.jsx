7a7522116ab59ca46d125798327f399d45b1b017
Retsos <GeorgeRets@gmail.com>
2025-03-11 02:02:02 +0200

    style fixxes in home

--- Project/Frontend/app/src/Components/Pages/Home.jsx
+++ Project/Frontend/app/src/Components/Pages/Home.jsx
@@ -8,19 +8,54 @@ import imgb from './../../assets/b.jpg'
 import imgc from './../../assets/EshopLogo.png'
 
 export default function Home() {
+    const [isSticky, setIsSticky] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // Calculate the halfway point of the initial visible page height
+            const initialPageHeight = window.innerHeight;
+
+            const twentyPercentPoint = initialPageHeight * 0.20; // 20%
+
+            // Get the current scroll position
+            const scrollPosition = window.scrollY;
+
+            // Check if the scroll position is past the halfway point
+            if (scrollPosition > twentyPercentPoint) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+
+        // Add the scroll event listener
+        window.addEventListener("scroll", handleScroll);
+
+        // Remove the listener when the component unmounts
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <>
-            <TopNavbar />
-            <Navbar />
+            <div className={`navbar-full-container ${isSticky ? 'sticky' : ''}`}>
+                <TopNavbar />
+                <Navbar />
+            </div>
+
             <div className="home-container">
 
-                <div className="photo-container my-5">
+                <div className="photo-container photo-container-1 my-5">
                     <img src={imga} alt={imgc} className="home-imges" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias necessitatibus architecto nisi dolorem iure minima temporibus amet nemo esse?</p>
+                    <div className="text-container">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias necessitatibus architecto nisi dolorem iure minima temporibus amet nemo esse?</p>
+                    </div>
                 </div>
-                <div className="photo-container my-5">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias necessitatibus architecto nisi dolorem iure minima temporibus amet nemo esse?</p>
+                <div className="photo-container photo-container-2 my-5">
+                    <div className="text-container">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus alias necessitatibus architecto nisi dolorem iure minima temporibus amet nemo esse?</p>
+                    </div>
                     <img src={imgb} alt={imgc} className="home-imges" />
                 </div>
 
